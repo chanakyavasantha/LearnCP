484f2265f93d8914b94fd121a98b773b392a1178
chanakya vasantha <93817654+chanakyavasantha@users.noreply.github.com>
2022-10-19 18:38:44 +0530

    Create solution.cpp

---
+++ Code Astra [Intermediate to Advanced]/Count Zeroes/solution.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    cout << n*pow(2,n-1);
+    return 0;
+}
